5422a35d6879a173bfb7baec128f820add9824de
obrockmole <86027400+obrockmole@users.noreply.github.com>
2026-04-28 15:09:35 -0400

    Still preparing for railway

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 import express from "express";
 import cors from "cors";
 import db from "./database.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get("/api/projects", (request, response) => {
     const query = `
